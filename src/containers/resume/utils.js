33e92610d4b06b9085fbd08f788559fe4d5f3e23
Aniket Rahane <aniketrahane121@gmail.com>
2023-11-01 21:30:44 +0530

    initial commit

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,28 @@
+export const data = {
+    experience: [
+      {
+        title: "Fresher",
+      },
+      
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "Shetkari Madhyamik Vidyalaya, Javalke",
+        description:
+          "10th Grade",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "Shree Ganesh Junior College, Korhale",
+        description:
+          "12th Grade in Science",
+      },
+      {
+        title: "B.Tech",
+        subTitle: "Sanjivani College of Engineering, Kopargaon",
+        description:
+          "Bachelor of technology in computer engineering",
+      },
+    ],
+  };
